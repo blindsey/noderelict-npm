3417af3f1617de6477c8595d7ee24250ab98de1c
Ben Lindsey <ben@carbonfive.com>
2011-08-27 15:32:48 -0700

    call the function directly, bump version to 0.0.2

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ var traces = [];
 function instrument( name, fn ) {
   var start = new Date(), error;
   try {
-    return fn.apply( this, arguments );
+    return fn( arguments );
   } catch( e ) {
     error = e;
     throw e;
